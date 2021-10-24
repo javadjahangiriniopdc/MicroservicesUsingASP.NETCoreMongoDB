23524d4da27d69a04577f4a7db65fd73a8ce512e
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-10-24 23:17:26 +0330

    add DataLauer and Business Layer to Project

---
+++ MicroservicesUsingASP.NETCoreMongoDB/Data/IStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroservicesUsingASP.NETCoreMongoDB.Entities;
+using MongoDB.Driver;
+
+namespace MicroservicesUsingASP.NETCoreMongoDB.Data
+{
+     public interface IStoreContext
+    {
+         IMongoCollection<Product> Products { get;  }
+    }
+}
---
+++ MicroservicesUsingASP.NETCoreMongoDB/Data/StoreContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroservicesUsingASP.NETCoreMongoDB.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Operations;
+
+namespace MicroservicesUsingASP.NETCoreMongoDB.Data
+{
+    public class StoreContext :IStoreContext
+    {
+        public StoreContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            StoreContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
